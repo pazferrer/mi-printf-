ed188dde75c3f4806e7bde4498ec7e5d90ebafae
pazferrer <162121573+pazferrer@users.noreply.github.com>
2024-03-06 17:51:30 +0100

    Add files via upload
    actuaizaci'on

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: pferrer- <pferrer-@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/16 18:02:50 by pferrer-          #+#    #+#             */
-/*   Updated: 2024/03/06 16:25:17 by pferrer-         ###   ########.fr       */
+/*   Updated: 2024/03/06 17:49:07 by pferrer-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,7 +62,7 @@ int	print_format(char s, va_list ap)
 	else if (s == 'x')
 		count += print_d((long)(va_arg(ap, unsigned int)), 16);
 	else if (s == 'X')
-		count += print_d((long)(va_arg(ap, unsigned int)), 17);
+		count += ft_printhexm((long)(va_arg(ap, unsigned int)));
 	else if (s == 'u')
 		count += print_d((long)(va_arg(ap, unsigned int)), 10);
 	else
